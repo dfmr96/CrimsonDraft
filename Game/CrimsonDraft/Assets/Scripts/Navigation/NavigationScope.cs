a11efe69a341b79736a01578e1d3e9d92dac9eff
David Marcano <dfmr96@gmail.com>
2026-04-02 20:00:39 -0300

    fix(navigation): register all interactable MonoBehaviours in NavigationScope

--- Game/CrimsonDraft/Assets/Scripts/Navigation/NavigationScope.cs
+++ Game/CrimsonDraft/Assets/Scripts/Navigation/NavigationScope.cs
@@ -50,6 +50,15 @@ namespace CrimsonDraft.Navigation
             builder.Register<DocumentController>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
             builder.RegisterComponentInHierarchy<ContainerView>();
             builder.Register<ContainerController>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
+
+            foreach (var t in FindObjectsByType<PoiInteractable>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+                builder.RegisterComponent(t);
+            foreach (var t in FindObjectsByType<DocumentInteractable>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+                builder.RegisterComponent(t);
+            foreach (var t in FindObjectsByType<DoorInteractable>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+                builder.RegisterComponent(t);
+            foreach (var t in FindObjectsByType<ContainerInteractable>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+                builder.RegisterComponent(t);
         }
     }
 }
