7fce5838b82e78ef58b9a5a37c078b41b556bdcf
David Marcano <dfmr96@gmail.com>
2026-06-28 12:44:56 -0300

    feat(navigation): configure AimFire input binding and IsAiming animator parameter

--- Game/CrimsonDraft/Assets/Tests/EditMode/RoomOrchestratorInitTests.cs
+++ Game/CrimsonDraft/Assets/Tests/EditMode/RoomOrchestratorInitTests.cs
@@ -167,6 +167,7 @@ namespace CrimsonDraft.Tests
             public InputAction OpenInventory          => null!;
             public InputAction OpenMap                => null!;
             public InputAction Aim                    => null!;
+            public InputAction AimFire                => null!;
             public InputAction Pause                  => null!;
             public InputAction Sprint                 => null!;
             public InputAction CombatNavigate         => null!;
