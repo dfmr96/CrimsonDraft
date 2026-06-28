9efcb48670b3d35a696526a7efb660d6d46fc39e
David Marcano <dfmr96@gmail.com>
2026-06-28 12:29:28 -0300

    feat(input): add AimFire action to IInputService and InputService

--- Game/CrimsonDraft/Assets/Scripts/Infrastructure/Input/InputService.cs
+++ Game/CrimsonDraft/Assets/Scripts/Infrastructure/Input/InputService.cs
@@ -36,6 +36,7 @@ namespace CrimsonDraft.Infrastructure.Input
         public InputAction OpenInventory { get; }
         public InputAction OpenMap { get; }
         public InputAction Aim { get; }
+        public InputAction AimFire { get; }
         public InputAction Pause { get; }
         public InputAction Sprint { get; }
         public InputAction CombatNavigate { get; }
@@ -80,6 +81,7 @@ namespace CrimsonDraft.Infrastructure.Input
             OpenInventory = this.gameplayMap[nameof(OpenInventory)];
             OpenMap       = this.gameplayMap[nameof(OpenMap)];
             Aim           = this.gameplayMap[nameof(Aim)];
+            AimFire       = this.gameplayMap[nameof(AimFire)];
             Pause         = this.gameplayMap[nameof(Pause)];
             Sprint        = this.gameplayMap[nameof(Sprint)];
 
